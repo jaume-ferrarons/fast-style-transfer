bea4ecdcc97a668c7672e4fecb87da24cc2bd7b1
Jaume Ferrarons <ckroxigor@gmail.com>
2017-07-29 12:10:06 +0200

    Can load all the images from a path

--- src/utils.py
+++ src/utils.py
@@ -22,10 +22,12 @@ def scale_img(style_path, style_scale):
 
 
 def get_media(src):
-    if src[-4:] == ".mp4":
+    if os.path.isdir(src):
+        return get_images(src)
+    elif src[-4:] == ".mp4":
         return sample_video(src)
     else:
-        print "Reading image"
+        print "Reading image", src
         return [get_img(src)]
 
 
@@ -58,24 +60,29 @@ def duration_to_seconds(duration):
     return h * 3600 + m * 60 + s
 
 
-def sample_video(in_path, n_samples=10):
+def get_images(dir_path):
+    """Loads all images in the path"""
+    images_path = [dir_path + fname for fname in list_files(dir_path)]
+    print "Loading", images_path
+    return map(get_img, images_path)
+
+def sample_video(in_path, n_samples=20):
+    """Extracts the samples from provided video and returns them"""
     TMP_VIDEO_DIR = '.tmp/'
+    TMP_VIDEO_SAMPLES = TMP_VIDEO_DIR + 'samples/'
     DURATION_FILE = TMP_VIDEO_DIR + 'duration'
     # Create TMP_VIDEO_DIR if doesn't exsist
-    if not os.path.exists(TMP_VIDEO_DIR):
-        os.makedirs(TMP_VIDEO_DIR)
+    if not os.path.exists(TMP_VIDEO_SAMPLES):
+        os.makedirs(TMP_VIDEO_SAMPLES)
     # Get the duration of the video
     cmd = ' '.join(["ffmpeg -i", in_path,
                     "2>&1 | grep Duration | awk '{print $2}' | tr -d , >", DURATION_FILE])
     subprocess.call(cmd, shell=True)
     duration = open(DURATION_FILE, 'r').readline().strip()
-    print "Video duration: ", duration
+    print "Video duration:", duration, "seconds"
     seconds = duration_to_seconds(duration)
     sample_cmd = ' '.join(["ffmpeg -i", in_path, '-vf fps=%d/%d' %
-                           (n_samples, seconds), TMP_VIDEO_DIR + "out%d.png"])
+                           (n_samples, seconds), TMP_VIDEO_SAMPLES + "out%d.png"])
     print "Taking sample"
     subprocess.call(sample_cmd, shell=True)
-    images_path = [
-        TMP_VIDEO_DIR + fname for fname in list_files(TMP_VIDEO_DIR) if fname[-4:] == ".png"]
-    print images_path
-    return map(get_img, images_path)
+    return get_images(TMP_VIDEO_SAMPLES)
